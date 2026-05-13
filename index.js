f20e4ad98b47ef1f348af76c8b960450e4d2ed48
k72106480-design <k72106480@gmail.com>
2026-05-13 05:01:11 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const TelegramBot = require('node-telegram-bot-api');
-
-const token = process.env.BOT_TOKEN;8981791917:AAGYMYOEhl89E7YE4BRHES0UhLAJi8MFPL8
-
-const bot = new TelegramBot(token, { polling: true });
-
-bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "🔥 أهلاً في Prime Subs");
-});
